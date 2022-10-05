9f42a446d5e1e5bb71c5c8744e53b84c167b1b03
SeungMiL <89256060+SeungMiL@users.noreply.github.com>
2022-10-05 23:42:56 +0900

    2

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,28 @@ const db = mysql.createConnection({
   database: "test",
 });
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.json("하잉 여기는 백엔드")
+})
+
+app.get('/books', (req, res) => {
+  const q = "SELECT * FROM test.books"
+  db.query(q, (err, data) => {
+    if(err) return res.json(err)
+    return res.json(data)
+  })
+})
+
+app.post('/books', (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+  const values = [req.body.title, req.body.desc, req.body.cover]
+
+  db.query(q, [values], (err, data) => {
+    if(err) return res.json(err)
+    return res.json("성공적으로 책을 생성했습니다")
+  })
 })
 
 app.listen(8800, () => {
